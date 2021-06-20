bb66fc5e161fab63a8d5622ed25577b0315f5ef0
Kamen Stefanov <37421799+kamenstefanov03@users.noreply.github.com>
2021-06-20 23:42:03 +0300

    Add files via upload

---
+++ DES/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int choice, num, i;
+
+        printf("1. \n");
+        printf("2. \n");
+        printf("3. \n");
+        printf("4. \n");
+        printf("Enter your choice :  ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("----");
+            scanf("%d", &num);
+
+            break;
+
+        case 2:
+            printf("----");
+            scanf("%d", &num);
+
+            break;
+
+
+        case 3:
+            printf("----");
+            scanf("%d", &num);
+
+           break;
+
+        case 4:
+
+            exit(0);
+
+
+       }
+}
+
